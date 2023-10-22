b5c91d4415f6bd95f7276365a86a74569f6cb723
David Bravo <bravodavid451@gmail.com>
2023-10-22 03:20:42 -0500

    feat(mapGeneration): :sparkles: implementar lógica inicial de generación procedural del mapa

---
+++ Game/code/mapGeneration/baseGeneration.py
@@ -0,0 +1,134 @@
+import random
+ 
+levelWidth = 100
+levelHeight = 50
+
+removeBlocks = 2386
+
+### Easy Procedural Generation: Drunken Walk Algorithm
+ 
+def getLevelRow():
+    return ['x'] * levelWidth
+ 
+def getWallLevel():
+    return [getLevelRow() for _ in range(levelHeight)]
+ 
+def drunkenWalkGenerator():
+    drunk = {
+        'removeBlocks': removeBlocks,
+        'padding': 2,
+        'x': int( levelWidth / 2 ),
+        'y': int( levelHeight / 2 )
+    }
+    
+    startCoordinate = [drunk['x'], drunk['y']]
+    
+    level = getWallLevel()
+    
+    p_x = random.randint(drunk['padding'], levelWidth - 1 - drunk['padding'])
+    p_y = random.randint(drunk['padding'], levelHeight - 1 - drunk['padding'])
+    level[p_y][p_x] = 'p'
+    
+    drunk['x'] = p_x
+    drunk['y'] = p_y
+    
+    while drunk['removeBlocks'] >= 0:
+        x = drunk['x']
+        y = drunk['y']
+        
+        if level[y][x] == 'x':
+            level[y][x] = ' '
+            drunk['removeBlocks'] -= 1
+        
+        roll = random.randint(1, 4)
+        
+        if roll == 1 and x > drunk['padding']:
+            drunk['x'] -= 1
+        if roll == 2 and x < levelWidth - 1 - drunk['padding']:
+            drunk['x'] += 1
+        if roll == 3 and y > drunk['padding']:
+            drunk['y'] -= 1
+        if roll == 4 and y < levelHeight - 1 - drunk['padding']:
+            drunk['y'] += 1
+    
+    level[y][x] = 'e'
+    endCoordinate = [x, y]
+    
+    return [level, startCoordinate, endCoordinate]
+
+### Easy Pathfinding: Breadth-First Algorithm
+ 
+def getNextMoves(x, y):
+    return {
+        'left':  [x-1, y], 
+        'right': [x+1, y],
+        'up':  [x, y-1],
+        'down':  [x, y+1]
+    }.values()
+ 
+def getShortestPath(level, startCoordinate, endCoordinate):
+    searchPaths = [[startCoordinate]]
+    visitedCoordinates = [startCoordinate]
+    
+    while searchPaths != []:
+        currentPath = searchPaths.pop(0)
+        currentCoordinate = currentPath[-1]
+        
+        currentX, currentY = currentCoordinate
+        
+        if currentCoordinate == endCoordinate:
+            return currentPath
+        
+        for nextCoordinate in getNextMoves(currentX, currentY):
+            nextX, nextY = nextCoordinate
+            
+            if nextX < 0 or nextX >= levelWidth:
+                continue
+            
+            if nextY < 0 or nextY >= levelHeight:
+                continue
+            
+            if nextCoordinate in visitedCoordinates:
+                continue
+            
+            if level[nextY][nextX] == 'x':
+                continue
+            
+            searchPaths.append(currentPath + [nextCoordinate])
+            visitedCoordinates += [nextCoordinate]
+    
+    return []
+ 
+### Improved Procedural Generation:
+### Drunken Walk + Breadth First Algorithm
+ 
+def generateLevels(amount):
+    return [drunkenWalkGenerator() for _ in range(amount)]
+ 
+def evaluateLevels(levels):
+    evaluationScores = []
+    
+    for generatedLevel, startCoordinate, endCoordinate in levels:
+        shortestSolution = getShortestPath(
+            generatedLevel, 
+            startCoordinate, 
+            endCoordinate
+        )
+        
+        evaluationScores.append(
+            [len(shortestSolution), generatedLevel]
+        )
+    
+    return evaluationScores
+ 
+def generateBestLevel(amountOfLevels):
+    levels = generateLevels(amountOfLevels)
+    
+    evaluationScores = evaluateLevels(levels)
+    
+    evaluationScores.sort()
+    evaluationScores.reverse()
+    
+    score, bestLevel = evaluationScores.pop(0)
+    
+    return bestLevel
